6d57437929f10bc1a038300a872ee776407bd9b9
alelievr <alelievr@student.42.fr>
2016-04-22 17:35:14 +0200

    not compiled playerManager.cs

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate() {
+		Vector2 mouvement = new Vector2();
+		
+	}
+}
