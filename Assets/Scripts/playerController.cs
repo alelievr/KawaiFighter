28ba1495c3b2f9dbfe52557367b1a2e882f3a260
alelievr <alelievr@student.42.fr>
2016-04-22 21:32:16 +0200

    player animation begin

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
+using System.IO.Ports;
 
 public class playerController : MonoBehaviour {
 	
-	public float jumpPower = 2.5F;
+	public float jumpPower = 10F;
 	public float gravityScale = 1F;
+	public float speed = 5F;
 	
 	private CharacterController	cc;
+	private Animator			anim;
+	private Vector3				inputVelocity;
+
+	//////////////////////////////////////////////
+	SerialPort	sp = new SerialPort("COM4", 115200);
+	//////////////////////////////////////////////
 
 	// Use this for initialization
 	void Start () {
 		cc = GetComponent< CharacterController >();
-		
+		anim = GetComponent< Animator >();
 	}
 	
 	// Update is called once per frame
@@ -20,16 +28,40 @@ public class playerController : MonoBehaviour {
 	
 	}
 	
+	public string ReadFromArduino (int timeout = 0) {
+		sp.ReadTimeout = timeout;        
+		try {
+			return sp.ReadLine();
+		}
+		catch (System.Exception) {
+			return null;
+		}
+	}
+	
+	void arduinoController() {
+		Debug.Log("arduino datas: " + ReadFromArduino());
+	}
+	
 	void FixedUpdate() {
-		Vector3 mouvement = new Vector3(0, CrossPlatformInputManager.GetAxis("Vertical"), CrossPlatformInputManager.GetAxis("Horizontal"));
+		inputVelocity.z = CrossPlatformInputManager.GetAxis("Horizontal") * speed;
 
 		if (cc.isGrounded) {
-			mouvement.y = 0;
-			if (CrossPlatformInputManager.GetButtonDown("Jump"))
-				mouvement.y = jumpPower;
+			if (CrossPlatformInputManager.GetButtonDown("Jump")) {
+				inputVelocity.y += jumpPower;
+				anim.SetBool("jump", true);
+			} else {
+				anim.SetBool("jump", false);
+				inputVelocity.y = 0;
+			}
 		}
-		mouvement.y -= gravityScale * Physics.gravity.y * Time.deltaTime;
-		if (mouvement != Vector3.zero)
-			cc.Move(mouvement);
+		if (inputVelocity.z < 0)
+			transform.localRotation = Quaternion.Euler(0, 180, 0);
+		else if (inputVelocity.z > 0)
+			transform.localRotation = Quaternion.Euler(0, 0, 0);
+		
+		inputVelocity.y += Physics.gravity.y * gravityScale * Time.deltaTime * 5;
+		if (inputVelocity != Vector3.zero)
+			cc.Move(inputVelocity * Time.deltaTime);
+		anim.SetFloat("speed", Mathf.Abs(cc.velocity.z));
 	}
 }
